96c6f42b2683bb4415fa3a5034417219223a0ea0
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-18 14:43:36 +0300

    +added: IGenericService in Core +added: NoDataDto in Shared

---
+++ JWTProject/JWTProject.Core/Services/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using JWTProject.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Core.Services
+{
+    public interface IGenericService<TEntity,TDto> where TEntity : class where TDto:class
+    {
+        Task<ResponseDto<TDto>> GetByIdAsync(int id);
+        Task<ResponseDto<IEnumerable<TDto>>> GetAllAsync();
+        Task<ResponseDto<TDto>> AddAsync(TEntity entity);
+        Task<ResponseDto<NoDataDto>> Update(TEntity entity);
+        Task<ResponseDto<NoDataDto>> Remove(TEntity entity);
+        Task<ResponseDto<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
+    }
+}
