8513110865e78e90bcd358bbbdb828481e6198c7
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-18 18:15:22 +0300

    +added: Authentication Service and DTO sources in Core Layer

---
+++ JWTProject/JWTProject.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using JWTProject.Core.DTOs;
+using JWTProject.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<ResponseDto<TokenDto>> CreateTokenByRefreshTokenAsync(string refreshToken);
+        Task<ResponseDto<NoDataDto>> RevokeRefreshTokenAsync(string refreshToken);
+        Task<ResponseDto<ClientTokenDto>> CreateTokenByClientAsync(ClientLoginDto clientLoginDto);
+    }
+}
