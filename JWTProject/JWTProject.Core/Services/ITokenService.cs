13a892c65f83dac09045bf051c77da5c6f49a5cb
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-18 14:58:11 +0300

    +added: ITokenService and Client in Core

---
+++ JWTProject/JWTProject.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using JWTProject.Core.Configurations;
+using JWTProject.Core.DTOs;
+using JWTProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
