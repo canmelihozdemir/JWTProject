ab3ebf4ba517a022a0c01c0f7db9abbf2902bc6b
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-19 11:30:00 +0300

    +added: UnitOfWork in Repo

---
+++ JWTProject/JWTProject.Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using JWTProject.Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
