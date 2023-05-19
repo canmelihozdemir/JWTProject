cc4a59e4fb734c8a25b4cf468e8dd92a87b669ba
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-19 10:32:29 +0300

    +refactored: Product Model in Core

--- JWTProject/JWTProject.Core/Models/Product.cs
+++ JWTProject/JWTProject.Core/Models/Product.cs
@@ -9,9 +9,9 @@ namespace JWTProject.Core.Models
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
     }
 }
