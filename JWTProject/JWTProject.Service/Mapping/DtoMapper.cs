9e3c2a5a8d57a0ca7e16a9e6c26e2ffe0a71ab15
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-19 15:08:33 +0300

    +loaded: AutoMapper +added: Mapping with AutoMapper in Service

---
+++ JWTProject/JWTProject.Service/Mapping/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using JWTProject.Core.DTOs;
+using JWTProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Service.Mapping
+{
+    class DtoMapper:Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto,Product>().ReverseMap();
+            CreateMap<UserAppDto,UserApp>().ReverseMap();
+        }
+    }
+}
