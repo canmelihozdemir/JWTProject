4918fdb14009c001d681b999c504bbbdd4b5baa5
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-19 10:17:57 +0300

    +added: IUserService in Core

---
+++ JWTProject/JWTProject.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using JWTProject.Core.DTOs;
+using JWTProject.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Core.Services
+{
+    public interface IUserService
+    {
+        Task<ResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<ResponseDto<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
