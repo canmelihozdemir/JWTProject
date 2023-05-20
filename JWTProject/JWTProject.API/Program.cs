170a350233d47b887f85e3027f6b8ebc0f8feef2
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-20 17:51:22 +0300

    +added: CustomTokenOption Configuration

--- JWTProject/JWTProject.API/Program.cs
+++ JWTProject/JWTProject.API/Program.cs
@@ -1,6 +1,14 @@
+using JWTProject.Shared.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+
+
+//
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+//
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
