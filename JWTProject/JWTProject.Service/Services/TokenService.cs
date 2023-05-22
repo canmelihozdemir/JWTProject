2dd0eb80904fe8eea2ffe3b703c88a82ebae6fd8
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-22 11:23:41 +0300

    +added: CreateRefreshToken Method in TokenService

---
+++ JWTProject/JWTProject.Service/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using JWTProject.Core.Configurations;
+using JWTProject.Core.DTOs;
+using JWTProject.Core.Models;
+using JWTProject.Core.Services;
+using JWTProject.Shared.Configurations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTProject.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _tokenOption;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
