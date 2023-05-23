6c93c32555b9c55ec65723fc879ecd93fd66ffab
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-23 14:13:23 +0300

    +added: AccessToken Methods in AuthService

--- JWTProject/JWTProject.Service/Services/AuthenticationService.cs
+++ JWTProject/JWTProject.Service/Services/AuthenticationService.cs
@@ -6,6 +6,7 @@ using JWTProject.Core.Services;
 using JWTProject.Core.UnitOfWorks;
 using JWTProject.Shared.DTOs;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -33,14 +34,43 @@ namespace JWTProject.Service.Services
         }
 
 
-        public Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        public async Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto)
         {
-            throw new NotImplementedException();
+            if (loginDto == null) throw new ArgumentNullException(nameof(loginDto));
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return ResponseDto<TokenDto>.Fail("Email or Password wrong",400,true);
+
+            if (!(await _userManager.CheckPasswordAsync(user,loginDto.Password))) return ResponseDto<TokenDto>.Fail("Email or Password wrong", 400, true);
+
+            var token = _tokenService.CreateToken(user);
+            var userRefreshToken = await _userRefreshTokenRepository.Where(x=>x.UserId==user.Id).SingleOrDefaultAsync();
+
+            if (userRefreshToken == null)
+            {
+                await _userRefreshTokenRepository.AddAsync(new UserRefreshToken { UserId = user.Id, Code = token.RefreshToken, Expiration = token.RefreshTokenExpiration });
+            }
+            else
+            {
+                userRefreshToken.Code = token.RefreshToken;
+                userRefreshToken.Expiration = token.RefreshTokenExpiration;
+            }
+
+            await _unitOfWork.CommitAsync();
+
+            return ResponseDto<TokenDto>.Success(token,200);
         }
 
-        public Task<ResponseDto<ClientTokenDto>> CreateTokenByClientAsync(ClientLoginDto clientLoginDto)
+        public ResponseDto<ClientTokenDto> CreateTokenByClient(ClientLoginDto clientLoginDto)
         {
-            throw new NotImplementedException();
+            var client = _clients.SingleOrDefault(x=>x.Id==clientLoginDto.ClientId && x.Secret==clientLoginDto.ClientSecret);
+
+            if(client == null)  return ResponseDto<ClientTokenDto>.Fail("ClientId or ClientSecret not found",404,true);
+
+            var token = _tokenService.CreateTokenByClient(client);
+
+            return ResponseDto<ClientTokenDto>.Success(token,200);
         }
 
         public Task<ResponseDto<TokenDto>> CreateTokenByRefreshTokenAsync(string refreshToken)
