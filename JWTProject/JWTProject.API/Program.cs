21d6bef1b36173c6761461bc947f631de5d3ef64
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-24 20:54:28 +0300

    +added: DI Container with Scoped in Program in API Layer +added: DbContext and Identity Configurations in Program

--- JWTProject/JWTProject.API/Program.cs
+++ JWTProject/JWTProject.API/Program.cs
@@ -1,25 +1,45 @@
 using JWTProject.Core.Configurations;
+using JWTProject.Core.Repositories;
+using JWTProject.Core.Services;
+using JWTProject.Core.UnitOfWorks;
+using JWTProject.Repository;
+using JWTProject.Repository.Repositories;
+using JWTProject.Service.Services;
 using JWTProject.Shared.Configurations;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-
-
 //
+
+builder.Services.AddScoped<IAuthenticationService,AuthenticationService>();
+builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<,>),typeof(GenericService<,>));
+
+builder.Services.AddDbContext<AppDbContext>(options=>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly("JWTProject.Repository");
+    });
+});
+
+
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
 //
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
