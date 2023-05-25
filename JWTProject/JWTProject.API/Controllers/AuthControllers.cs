37eadde73a0cff2f9ecb95238237df7fddb37420
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-25 13:01:51 +0300

    +added: AuthController and written post methods in API

---
+++ JWTProject/JWTProject.API/Controllers/AuthControllers.cs
@@ -0,0 +1,26 @@
+﻿using JWTProject.Core.DTOs;
+using JWTProject.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTProject.API.Controllers
+{
+    public class AuthControllers : CustomBaseControllers
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthControllers(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost] public async Task<IActionResult> CreateTokenAsync(LoginDto loginDto) => ActionResultInstance(await _authenticationService.CreateTokenAsync(loginDto));
+
+        [HttpPost] public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto) => ActionResultInstance(_authenticationService.CreateTokenByClient(clientLoginDto));
+
+        [HttpPost] public async Task<IActionResult> RevokeRefreshTokenAsync(RefreshTokenDto refreshTokenDto) => ActionResultInstance(await _authenticationService.RevokeRefreshTokenAsync(refreshTokenDto.Token!));
+
+        [HttpPost] public async Task<IActionResult> CreateTokenByRefreshTokenAsync(RefreshTokenDto refreshTokenDto) => ActionResultInstance(await _authenticationService.CreateTokenByRefreshTokenAsync(refreshTokenDto.Token!));
+
+    }
+}
---
+++ JWTProject/JWTProject.API/Controllers/CustomBaseControllers.cs
@@ -0,0 +1,18 @@
+﻿using JWTProject.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTProject.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomBaseControllers : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(ResponseDto<T> response) where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
