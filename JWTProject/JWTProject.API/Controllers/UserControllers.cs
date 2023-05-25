9be237b215c36e156fe9652196aac2ebc4725d5c
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-25 13:36:40 +0300

    +added: UserController and its some methods in API

---
+++ JWTProject/JWTProject.API/Controllers/UserControllers.cs
@@ -0,0 +1,26 @@
+﻿using JWTProject.Core.DTOs;
+using JWTProject.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserControllers : CustomBaseControllers
+    {
+        private readonly IUserService _userService;
+
+        public UserControllers(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost] public async Task<IActionResult> CreateUserAsync(CreateUserDto createUserDto) => ActionResultInstance(await _userService.CreateUserAsync(createUserDto));
+
+        [Authorize][HttpGet] public async Task<IActionResult> GetUserAsync() => ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity!.Name!));
+
+
+    }
+}
