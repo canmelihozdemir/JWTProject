7e3b63b0eb416d4769f9d765a2a400621447e8be
Melih Özdemir <canmelihozdemir@gmail.com>
2023-05-29 11:59:12 +0300

    +added: Test Controllers with GET in Mini APIs

---
+++ JWTProject/JWTMiniApp1.API/Controllers/StockControllers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace JWTMiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockControllers : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity!.Name;
+
+            var userId = User.Claims.FirstOrDefault(x=>x.Type==ClaimTypes.NameIdentifier);
+
+            return Ok($"Stock process=>     UserName: {userName} - UserId: {userId}");
+        }
+    }
+}
